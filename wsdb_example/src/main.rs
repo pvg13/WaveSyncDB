7d809d01b6e6fe8998f0d91af1cd6b6aefcfc5ce
pvazquez <pablo@vgomis.com>
2025-12-15 17:16:48 +0100

    Added p2p example

--- wsdb_example/src/main.rs
+++ wsdb_example/src/main.rs
@@ -1,38 +1,36 @@
-mod schema;
 mod model;
+mod schema;
 
+use std::{error::Error, thread::sleep, time::Duration};
 
-use std::{error::Error, time::Duration};
-
-use diesel::{prelude::*, r2d2::{ConnectionManager, Pool}};
+use diesel::{
+    prelude::*,
+    r2d2::{ConnectionManager, Pool, PooledConnection},
+    sqlite::Sqlite,
+};
 use diesel_migrations::{EmbeddedMigrations, MigrationHarness};
 use dotenvy::dotenv;
-use wavesyncdb::prelude::WaveSyncInstrument;
 use model::Task;
+use tokio::time::sleep_until;
 use wavesyncdb::instrument::dialects::DialectType;
+use wavesyncdb::prelude::WaveSyncInstrument;
 
 const MIGRATIONS: EmbeddedMigrations = diesel_migrations::embed_migrations!();
 
-
-#[tokio::main]
-pub async fn main() -> Result<(), Box<dyn Error>> {
-
-    dotenv().ok();
-    env_logger::init();
-
-    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-
+fn start_database_client(
+    database_url: String,
+) -> Result<PooledConnection<ConnectionManager<SqliteConnection>>, Box<dyn Error>> {
     let manager = ConnectionManager::<SqliteConnection>::new(database_url);
     let pool = Pool::builder()
-        .max_size(16)                               // tamaño del pool
+        .max_size(16) // tamaño del pool
         .min_idle(Some(2))
         .connection_timeout(Duration::from_secs(3)) // timeout al pedir conexión
         .build(manager)?;
 
-    
     let mut conn = pool.get()?;
     // Run migrations if needed
-    conn.run_pending_migrations(MIGRATIONS).expect("Error running migrations");
+    conn.run_pending_migrations(MIGRATIONS)
+        .expect("Error running migrations");
 
     let (tx, rx) = tokio::sync::mpsc::channel(100);
 
@@ -42,6 +40,41 @@ pub async fn main() -> Result<(), Box<dyn Error>> {
 
     tokio::spawn(async move {
         wavesync_engine.run().await;
+    });
+
+    Ok(conn)
+}
+
+#[tokio::main]
+pub async fn main() -> Result<(), Box<dyn Error>> {
+    dotenv().ok();
+    env_logger::init();
+
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    // Create Alice
+
+    let mut alice = start_database_client(format!("{}.1.db", database_url)).unwrap();
+    let mut bob = start_database_client(format!("{}.2.db", database_url)).unwrap();
+
+    std::thread::spawn(move || {
+        loop {
+            use schema::tasks::dsl::*;
+
+            let results = tasks
+                .limit(5)
+                .select(Task::as_select())
+                .load(&mut bob)
+                .expect("Error loading posts");
+
+            println!("Displaying {} posts", results.len());
+            for post in results {
+                println!("{}", post.title);
+                println!("-----------\n");
+                println!("{:?}", post.description);
+            }
+            sleep(Duration::from_secs(1));
+        }
     });
 
     // Insert a new task
@@ -65,9 +98,8 @@ pub async fn main() -> Result<(), Box<dyn Error>> {
                 };
                 diesel::insert_into(schema::tasks::table)
                     .values(&new_task)
-                    .execute(&mut conn)
+                    .execute(&mut alice)
                     .expect("Error inserting new task");
-
             }
             Err(error) => {
                 println!("error: {}", error);
